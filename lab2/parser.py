94b9c88dcf56aaafb6a1df602f06b28a27e11835
Rais <raisperm@mail.ru>
2019-03-13 23:00:06 +0500

    Lab2_fix

--- lab2/parser.py
+++ lab2/parser.py
@@ -13,6 +13,18 @@ base_url = 'https://lenta.ru'
 locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
 locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
 c = sqlite3.connect('db')
+header = {
+    # 'Host': 'lenta.ru',
+    # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0',
+    # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    # 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    # 'Accept-Encoding': 'gzip, deflate, br',
+    # 'DNT': '1',
+    # 'Connection': 'keep-alive',
+    'Cookie': 'lid=vAsAAMU4iVxtAsOTAUa8BwB=',
+    # 'Upgrade-Insecure-Requests': '1',
+    # 'If-None-Match': 'W/"f5dc2923bf1f59f4bc9b92f59723b026"'
+}
 
 
 def parse_news_page(news_url):
@@ -20,7 +32,7 @@ def parse_news_page(news_url):
     if news_exists(news_url):
         return
 
-    resp = requests.get(url)
+    resp = requests.get(url, headers=header)
     news_soup = BeautifulSoup(resp.text, u"lxml")
 
     posted_at = news_soup.select(u'.b-topic__info .g-date')
@@ -71,14 +83,30 @@ def news_exists(url):
     cur.close()
     return result
 
+
 last_date = datetime.datetime.now()
 curr_date = last_date
 
-while (curr_date - last_date).days < 365:
-    archive_url = base_url + '/' + '/'.join([str(last_date.year), str(last_date.month), str(last_date.day)])
-    resp = requests.get(archive_url)
+
+import requests
+
+while (curr_date - last_date).days < 3:
+    if len(str(last_date.month)) == 1:
+        checkMonth = "0"
+    else:
+        checkMonth = ""
+
+    if len(str(last_date.day)) == 1:
+        checkDay = "0"
+    else:
+        checkDay = ""
+
+    archive_url = base_url + '/news' + '/' + '/'.join(
+        [str(last_date.year), checkMonth + str(last_date.month), checkDay + str(last_date.day)])
+    resp = requests.get(archive_url, headers=header)
     archive_soup = BeautifulSoup(resp.text, "lxml")
-    archive_items = archive_soup.select('div.item div.titles h3 a[href]')
+    archive_items = archive_soup.select('.b-layout_archive div.item div.titles h3 a[href]')
     for item in archive_items:
+        print(item['href'])
         parse_news_page(item['href'])
-last_date = last_date - datetime.timedelta(1)
+    last_date = last_date - datetime.timedelta(1)
